b06f04409470c8f0104ef61329aa4c82dda029a7
git-navin <152207371+git-navin@users.noreply.github.com>
2023-12-12 16:24:53 +0530

    created customer table and inserted 7 rows into it

--- SQL_Practice/Customer_table.sql
+++ SQL_Practice/Customer_table.sql
@@ -5,14 +5,24 @@ CREATE TABLE Customers (
     last_name VARCHAR2(50),
     email VARCHAR2(100),
     phone_number VARCHAR2(20),
-    address VARCHAR2(255)
+    address VARCHAR2(255),
+    city VARCHAR2(25),
+    country VARCHAR(25)
 );
 
+SELECT * FROM customers;
 -- Insert sample data into the Customers table
-INSERT INTO Customers VALUES (1, 'John', 'Doe', 'john.doe@email.com', '123-456-7890', '123 Main St');
-INSERT INTO Customers VALUES (2, 'Jane', 'Smith', 'jane.smith@email.com', '987-654-3210', '456 Oak St');
--- Add more sample data as needed
 
+INSERT INTO Customers (customer_id,first_name,last_name,email,phone_number,address,city,country)VALUES(1, 'John', 'Doe', 'john.doe@email.com', '123-456-7890', '123 Main St', 'New York', 'USA');
+INSERT INTO Customers (customer_id,first_name,last_name,email,phone_number,address,city,country)VALUES(2, 'Jane', 'Smith', 'jane.smith@email.com', '987-654-3210', '456 Oak St', 'Los Angeles', 'USA');
+INSERT INTO Customers (customer_id,first_name,last_name,email,phone_number,address,city,country)VALUES(3, 'Alice', 'Johnson', 'alice.johnson@email.com', '555-555-5555', '789 Elm St', 'Chicago', 'USA');
+INSERT INTO Customers VALUES(6, 'Robert', 'Johnson', 'robert.johnson@email.com', '555-123-4567', '101 Pine St', 'San Francisco', 'USA');
+INSERT INTO Customers VALUES(4, 'Mary', 'Williams', 'mary.williams@email.com', '123-789-0123', '789 Maple St', 'Miami', 'USA');
+INSERT INTO Customers VALUES(5, 'David', 'Miller', 'david.miller@email.com', '987-654-3210', '555 Oak St', 'Dallas', 'USA');
+INSERT INTO Customers VALUES(7, 'Eva', 'Garcia', 'eva.garcia@email.com', '444-555-6666', '222 Cedar St', 'Phoenix', 'USA');
+
+-- Add more sample data as needed
+SELECT * FROM customers;
 -- Create an Orders table
 CREATE TABLE Orders (
     order_id NUMBER PRIMARY KEY,
